c52e821b0d9dd57e1b795a66f32503439acfc6fc
albertjacobsz <dekater403@gmail.com>
2023-11-19 02:48:04 +0800

    changed flight info button to be pressable

--- components/FlightInfoButton.tsx
+++ components/FlightInfoButton.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable} from 'react-native';
 import generalStyles from './../styles/GeneralStyles';
 
 import FlightInfoButtonStyles from './styles/FlightInfoButtonStyles';
 function FlightInfoButton(): JSX.Element {
+  const clicked = () => {
+    console.log('hello world');
+  };
   return (
-    <View style={FlightInfoButtonStyles.container}>
+    <Pressable onPress={clicked} style={FlightInfoButtonStyles.container}>
       <Text style={FlightInfoButtonStyles.whiteText}>CX691</Text>
       <View style={[generalStyles.row, {flex: 0}]}>
         <View style={FlightInfoButtonStyles.leftSideAlign}>
@@ -23,7 +26,7 @@ function FlightInfoButton(): JSX.Element {
           <Text style={FlightInfoButtonStyles.whiteTextBig}>HKG</Text>
         </View>
       </View>
-    </View>
+    </Pressable>
   );
 }
 export default FlightInfoButton;
